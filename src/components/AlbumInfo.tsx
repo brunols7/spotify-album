33affc2239f68658573396b5ab7cd8af2c97aaa4
Bruno Leonardo <bruno.lsilva1508@gmail.com>
2025-11-10 19:43:12 -0300

    feat: add buttons
    + hero-icons

--- src/components/AlbumInfo.tsx
+++ src/components/AlbumInfo.tsx
@@ -1,6 +1,33 @@
 import '../styles/AlbumInfo.css'
+import { PlayIcon, HeartIcon, ShareIcon } from '@heroicons/react/24/outline';
 
 export function AlbumInfo(props: {isNew: boolean | false, title: string, artist: string, bio: string, link: string}){
+
+    function PlayButton(){
+        
+        return (
+            <button className='play-btn'>
+                <PlayIcon style={{ width: 20, height: 20 }} /> {}
+                Ouvir Agora
+            </button>
+        );
+    }
+
+    function FavoriteButton(){
+        return (
+            <button className='favorite-btn'>
+                <HeartIcon style={{ width: 20, height: 20 }} /> {}
+                Adicionar à playlist
+            </button>
+        )
+    }
+
+    function ShareButton(){
+        return <button className='share-btn'>
+            <ShareIcon style={{ width: 20, height: 20 }} />
+            Compartilhar
+        </button>
+    }
 
     return (
         <div className="info">
@@ -8,6 +35,11 @@ export function AlbumInfo(props: {isNew: boolean | false, title: string, artist:
             <h1>{props.title}</h1>
             <h3>by {props.artist}</h3>
             <p className="bio">{props.bio}</p>
+            <div className="buttons">
+                <PlayButton/>
+                <FavoriteButton/>
+                <ShareButton/>
+            </div>
         </div>
     );
 
